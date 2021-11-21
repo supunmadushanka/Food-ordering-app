7a70214b309dd824141ff63d7b2092a5d14cd21a
supun <supunmadushanka19980822@gmail.com>
2021-11-21 22:13:05 +0530

    bug-fixed

--- client/src/app/components/menu-item/update-item/update-item.component.ts
+++ client/src/app/components/menu-item/update-item/update-item.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, Validators } from '@angular/forms'
 import { Router,ActivatedRoute } from '@angular/router'
 import {HotelService} from '../../../services/hotel.service'
 import {MenuService} from '../../../services/menu.service'
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-update-item',
@@ -13,12 +14,29 @@ export class UpdateItemComponent implements OnInit {
 
   hotelId;
   itemId
+  public menu : [];
+  name : string
+  desc : string
+  price : string
 
-  constructor(private _menuService : MenuService,private fb: FormBuilder,private router: Router, private route : ActivatedRoute,private _hotelservice : HotelService) { }
+  constructor(private location: Location,private _menuService : MenuService,private fb: FormBuilder,private router: Router, private route : ActivatedRoute,private _hotelservice : HotelService) { }
 
   ngOnInit(): void {
     this.hotelId=this._hotelservice.getHotelId();
     this.itemId=this.route.snapshot.paramMap.get("id");
+
+    this._menuService.getMenu(this.itemId,this.hotelId)
+      .subscribe((data) => {
+        this.menu = data;
+        this.name = data.name;
+        this.desc = data.desc;
+        this.price = data.price;
+      },
+        error => {
+          console.error('error', error)
+        }
+      )
+
   }
 
   addItem = this.fb.group({
@@ -33,6 +51,7 @@ export class UpdateItemComponent implements OnInit {
     .subscribe(
       response=>{
         console.log('Success!', response);
+        this.location.back();
       },
       error=>{
         console.error('Error!', error)
