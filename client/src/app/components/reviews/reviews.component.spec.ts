ef600f3a733d8db6ae4de9dfd9570155f1764dfc
inoshi <kavindir127@gmail.com>
2021-11-28 00:45:09 +0530

    rating

--- client/src/app/components/reviews/reviews.component.spec.ts
+++ client/src/app/components/reviews/reviews.component.spec.ts
@@ -23,3 +23,26 @@ describe('ReviewsComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+
+describe('RatingComponent', () => {
+  let component: RatingComponent;
+  let fixture: ComponentFixture<RatingComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [RatingComponent]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RatingComponent);
+    component = fixture.componentInstance;
+  });
+
+  describe('when the rating has no remainder', () => {
+    it('shows five empty stars when the rating is zero', () => {
+      component.rating = 0;
+      fixture.detectChanges();
+
+      expect(getEmptyStars().length).toEqual(5);
+    });
