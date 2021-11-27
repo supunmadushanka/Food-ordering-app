0bab7072e423875253265120d5eb988d3cf9bec3
Geeth-Rath <geeth4862@gmail.com>
2021-11-27 20:03:55 +0530

    asdded css

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public user = [];
+  username: string
+  email: string
+
+  constructor(private fb: FormBuilder, private router: Router, private route: ActivatedRoute, private _profileservice: ProfileService) { }
 
   ngOnInit(): void {
+    this._profileservice.getUser(this._profileservice.getUserId())
+      .subscribe((data) => {
+        this.user = data;
+        this.username = data.username
+        this.email = data.email
+      },
+        error => {
+          console.error('error', error)
+        }
+      )
+  }
+
+  profile = this.fb.group({
+    username: [''],
+    email: ['']
+  })
+
+  itemsubmit() {
+    this._profileservice.updateUser(this._profileservice.getUserId(), this.profile.value)
+      .subscribe(
+        response => {
+          console.log('Success!', response);
+          localStorage.setItem('order-my-food-username', this.profile.value.username);
+          localStorage.setItem('order-my-food-email',this.profile.value.email);
+          this.Success();
+        },
+        error => {
+          console.error('Error!', error)
+        }
+      )
+  }
+
+  Success = () => {
+    Swal.fire({
+      icon: 'success',
+      title: 'Updated successfully',
+      html: 'Redirecting to the profile...',
+      timer: 3000,
+      timerProgressBar: true,
+      showConfirmButton: false,
+      willOpen: () => {
+      }
+    }).then((result) => { })
   }
 
 }
